79467933b384bdd75ce972690dc4056e5ce53d16
anuragsingh1999 <anuragbirendrasingh@gmail.com>
2024-12-06 11:30:56 +0000

    data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";//treat all JS code as newer version
+
+//alert("Hello") we are using nodejs,not browser
+//alert(3+3)
+
+let name = "anurag"
+let age = 22
+let isLoggedIN = false
+
+// number => 2 to power 53
+// bigint => 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => for finding uniquness
+
+
+
+//object 
+
+console.log(typeof "anurag");
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
